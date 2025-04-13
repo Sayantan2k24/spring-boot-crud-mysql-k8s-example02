0be156e0116079bb58018f06f7f675981a52e958
Sayantan <sayantansamanta12102001@gmail.com>
2025-04-13 19:20:36 +0530

    feat: Add base version with /health and /products

---
+++ src/main/java/com/javatechie/crud/example/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.javatechie.crud.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    private static final String VERSION = "v1.0.0"; // change this per version
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Service is up and running! Version: " + VERSION;
+    }
+}
